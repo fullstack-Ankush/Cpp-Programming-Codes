3ffdfefb39fc84f9b9307dc32cfce63b6f4a6e00
Ankush <24cs118@lingayasvidyapeeth.edu.in>
2026-09-21 19:52:27 +0530

    assignment 2 completed

---
+++ Assignment 2/solutions.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // solution 1
+    cout<<"Hello MySirG"<<endl;
+
+    // solution 2 
+    cout<<"Hello"<<endl<<"MySirG"<<endl;
+
+    // solution 3
+    int x ,y;
+    cin>>x;
+    cin>>y;
+    cout<<x+y;
+
+    // solution 4
+    int area,r;
+    cin>>r;
+    area = 3.14*r*r;
+    cout<<"Area of circle is "<<area<<endl;
+
+
+    // solution 5
+    int l,b,h;
+    cin>>l>>b>>h;
+    int volume = l*b*h;
+}
